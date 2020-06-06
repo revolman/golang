30f24d1ad6fae5dc3c3f847d7f0bfe166fcf5ddc
Ivn Revolman <revolman.eis@gmail.com>
2020-06-07 01:39:57 +0300

    ch8 clockwall есть вывод

--- ch8/clockwall_ex8.1/clockwall.go
+++ ch8/clockwall_ex8.1/clockwall.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
 	"strings"
+	"text/tabwriter"
+	"time"
 )
 
 // ZoneInfo имя таймзоны и адресс сервера
@@ -19,7 +20,7 @@ type ZoneInfo struct {
 const format = ("%v\t%v\n")
 
 func main() {
-	var timeZones []*ZoneInfo // все полученный зоны
+	var timeZones []*ZoneInfo // все полученные зоны
 
 	if len(os.Args) < 1 {
 		fmt.Printf("Введите аргументы:\nname=address:port\n")
@@ -38,7 +39,7 @@ func main() {
 }
 
 func clockWall(tz []*ZoneInfo) {
-	// tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
 	fmt.Printf(format, "Зона", "Время")
 	fmt.Printf(format, "----", "-----")
 
@@ -48,27 +49,27 @@ func clockWall(tz []*ZoneInfo) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		var tm = &bytes.Buffer{}
 
-		if _, err := io.Copy(tm, conn); err != nil {
-			log.Fatal(err)
-		}
-		// if _, err := tm.ReadFrom(conn); err != nil {
-		// 	log.Fatal(err)
-		// }
-
-		line, err := tm.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("\r%v\t%v", zone.Name, line)
+		go timePrint(conn, zone)
 
 		// go refreshTime(tw, conn, zone)
 	}
-	// tw.Flush()
-	// fmt.Println("Ghbdtn")
+	tw.Flush()
 
+	time.Sleep(5 * time.Minute)
+}
+
+// в качестве решения нужно попробовать обновлять время в структуре для каждого
+// отдельного *ZoneInfo, а в главной функции выводить результат в бесконечном цикле
+func timePrint(conn net.Conn, zone *ZoneInfo) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	for {
+		for scanner.Scan() {
+			text := scanner.Text()
+			fmt.Fprintf(os.Stdout, "\r%s: %s", zone.Name, text)
+		}
+	}
 }
 
 // func refreshTime(tw *tabwriter.Writer, conn net.Conn, zone *ZoneInfo) {
